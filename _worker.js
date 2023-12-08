d88b1e685c9f87530f3ee29d0dfdabed79e9a750
zcxw <101062813+zcxw-code@users.noreply.github.com>
2023-12-08 08:44:14 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,16 +1,21 @@
 export default {
   async fetch(request, env, ctx) {
-
-    if (request.method != "GET") {
-      return new Response("Error 405: Method not allowed", new Response("", { status: 405 }))
-    }
-
-
     const default_redirect = "about:blank"; // only url
     const someHost = "https://someHost.com"; // url 
     const regex = /^\/?[0-9a-zA-Z]{8}$/;
     const onUrl = new URL(request.url);
     const url = new URL(someHost + onUrl.pathname);
+    const status_path = 'm/status'
+    if (onUrl.pathname.endsWith(status_path)){
+      return new Response(`<body style="background: #fffff0;font-size: 100px;text-align: center;/* text-align-last: center; */position:fixed;top: 50%;bottom: 0;left: 0;right: 0;width: -webkit-fill-available;height: -webkit-fill-available;" class="i-ua_narrow">🤡</body>`, new Response("", { status: 200, headers: {"content-type": "text/html;charset=UTF-8"}}))
+
+    }
+    if (request.method != "GET") {
+
+      return new Response("Error 405: Method not allowed", new Response("", { status: 405 }))
+    }
+    
+
 
     const checkOnTag = function () {
       return regex.test(onUrl.pathname)
